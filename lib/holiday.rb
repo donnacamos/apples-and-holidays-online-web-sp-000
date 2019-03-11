2323b3f763f19e1116d1956ef2640ae11024112d
Donna Amos <donnacamos.developer@gmail.com>
2019-03-11 02:53:07 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -83,7 +83,7 @@ def all_holidays_with_bbq(holiday_hash)
 holiday_hash.each do |season, holiday_supplies_hash| 
   holiday_supplies_hash.each do |holiday, supplies| 
     if supplies.include? ("BBQ")
-      
+      bbq_array << 
 end
 
 
